5471a0ca72d132654e4afa0e7d4aa6456df3aaaa
Karthi-mr <mkarthim1998@gmail.com>
2026-01-06 12:27:41 +0530

    implemented product 1

---
+++ 05-ecommerce-app/product/src/main/java/com/preflearn/ecommerce/product/Product.java
@@ -0,0 +1,32 @@
+package com.preflearn.ecommerce.product;
+
+import com.preflearn.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private double availableQuantity;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
