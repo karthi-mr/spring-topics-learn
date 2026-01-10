4bbf3dbbbcb08f79c68ac4eff3906d4b2e60d20f
Karthi-mr <mkarthim1998@gmail.com>
2026-01-10 12:54:04 +0530

    chat response and metadata

--- 06-spring-ai/SpringAICode/src/main/java/com/preflearn/SpringAICode/OpenAIController.java
+++ 06-spring-ai/SpringAICode/src/main/java/com/preflearn/SpringAICode/OpenAIController.java
@@ -1,6 +1,7 @@
 package com.preflearn.SpringAICode;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,12 @@ public class OpenAIController {
     public String getAnswer(@PathVariable String message) {
 
         // String response = chatClient.call(message);
-        String response = chatClient.prompt(message).call().content();
+        ChatResponse chatResponse = chatClient.prompt(message).call().chatResponse();
+        assert chatResponse != null;
+        String response = chatResponse.getResult().getOutput().getText();
+        System.out.println("Model: " + chatResponse.getMetadata().getModel());
+        System.out.println("Rate limit: " + chatResponse.getMetadata().getRateLimit());
+        System.out.println("Usage: " + chatResponse.getMetadata().getUsage());
 
         return response;
     }
