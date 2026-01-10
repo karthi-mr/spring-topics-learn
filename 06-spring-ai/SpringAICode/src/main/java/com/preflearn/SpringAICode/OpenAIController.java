8b250296f4133f43e4d7c6650ac4575e75d4fb55
Karthi-mr <mkarthim1998@gmail.com>
2026-01-10 18:53:36 +0530

    prompt template and vector embeddings

--- 06-spring-ai/SpringAICode/src/main/java/com/preflearn/SpringAICode/OpenAIController.java
+++ 06-spring-ai/SpringAICode/src/main/java/com/preflearn/SpringAICode/OpenAIController.java
@@ -1,27 +1,39 @@
 package com.preflearn.SpringAICode;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
 public class OpenAIController {
 
     private ChatClient chatClient;
+    private ChatMemory chatMemory;
+    private EmbeddingModel embeddingModel;
 
     /* public OpenAIController(OpenAiChatModel chatModel) {
         this.chatClient = ChatClient.create(chatModel);
     } */
 
-    public OpenAIController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public OpenAIController(ChatClient.Builder builder, EmbeddingModel embeddingModel) {
+        this.chatMemory = MessageWindowChatMemory.builder().build();
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+        this.embeddingModel = embeddingModel;
     }
 
-    @GetMapping("/{message}")
+    @GetMapping("/search/{message}")
     public String getAnswer(@PathVariable String message) {
 
         // String response = chatClient.call(message);
@@ -33,5 +45,42 @@ public class OpenAIController {
         System.out.println("Usage: " + chatResponse.getMetadata().getUsage());
 
         return response;
+    }
+
+    @PostMapping("/recommend")
+    public String recommend(@RequestParam String type, @RequestParam String year, @RequestParam String language) {
+
+        String template = """
+                I want to watch a {type} movie tonight with good rating,
+                looking for movies around this year {year}.
+                The language im looking for is {lang}.
+                Suggest one specific movie and tell me the case and length of the movie.
+                
+                response format should be:
+                1. Movie name
+                2. basic plot
+                3. case
+                4. length
+                5. IMDB rating
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Prompt prompt = promptTemplate.create(Map.of(
+                "type", type,
+                "year", year,
+                "lang", language
+        ));
+
+        String response = chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+
+        return response;
+    }
+
+    @PostMapping("/embedding")
+    public float[] embedding(@RequestParam String text) {
+        return embeddingModel.embed(text);
     }
 }
