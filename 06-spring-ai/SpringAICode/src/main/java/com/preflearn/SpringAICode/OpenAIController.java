62d394a206d4c94b7691c342cb8baa503c6ab054
Karthi-mr <mkarthim1998@gmail.com>
2026-01-10 19:28:23 +0530

    cosine similarity

--- 06-spring-ai/SpringAICode/src/main/java/com/preflearn/SpringAICode/OpenAIController.java
+++ 06-spring-ai/SpringAICode/src/main/java/com/preflearn/SpringAICode/OpenAIController.java
@@ -3,7 +3,6 @@ package com.preflearn.SpringAICode;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -82,5 +81,22 @@ public class OpenAIController {
     @PostMapping("/embedding")
     public float[] embedding(@RequestParam String text) {
         return embeddingModel.embed(text);
+    }
+
+    @PostMapping("/similarity")
+    public double getSimilarity(@RequestParam String text1, @RequestParam String text2) {
+        float[] embedding1 = embeddingModel.embed(text1);
+        float[] embedding2 = embeddingModel.embed(text2);
+
+        double dotProduct = 0,
+                norm1 = 0,
+                norm2 = 0;
+
+        for (int i = 0; i < embedding2.length; i++) {
+            dotProduct += embedding1[i] * embedding2[i];
+            norm1 += Math.pow(embedding1[i], 2);
+            norm2 += Math.pow(embedding2[i], 2);
+        }
+        return (dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2))) * 100;
     }
 }
